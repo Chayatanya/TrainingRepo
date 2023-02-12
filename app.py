6af1956fecc020fcce14216dba5ed3d6324288b7
Chayatanya <deshmankar.c@gmail.com>
2023-02-12 15:06:54 +0000

    commit from dev1

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
- everyone
- How are you?
- am doing good. Hope you are doing good too.
+Print("everyone
+ How are you?")
+Print("am doing good. Hope you are doing good too.")
+Print("From Second Branch dev1")
+
